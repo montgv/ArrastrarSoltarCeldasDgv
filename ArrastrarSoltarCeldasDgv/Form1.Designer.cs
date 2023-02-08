48f5d097a8d9e036caa3cb92748deca2baac77df
Montse <montserrat.garrido.220@estudiante.iesoretania.es>
2023-02-08 17:06:59 +0100

    Agregar archivos de proyecto.

---
+++ ArrastrarSoltarCeldasDgv/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ArrastrarSoltarCeldasDgv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.dgv1Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgv2 = new System.Windows.Forms.DataGridView();
+            this.dgv2Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lbTexto = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv1
+            // 
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgv1Nombre});
+            this.dgv1.Location = new System.Drawing.Point(26, 110);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.RowHeadersWidth = 62;
+            this.dgv1.RowTemplate.Height = 28;
+            this.dgv1.Size = new System.Drawing.Size(335, 222);
+            this.dgv1.TabIndex = 0;
+            // 
+            // dgv1Nombre
+            // 
+            this.dgv1Nombre.HeaderText = "Nombre";
+            this.dgv1Nombre.MinimumWidth = 8;
+            this.dgv1Nombre.Name = "dgv1Nombre";
+            this.dgv1Nombre.Width = 150;
+            // 
+            // dgv2
+            // 
+            this.dgv2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgv2Nombre});
+            this.dgv2.Location = new System.Drawing.Point(432, 110);
+            this.dgv2.Name = "dgv2";
+            this.dgv2.RowHeadersWidth = 62;
+            this.dgv2.RowTemplate.Height = 28;
+            this.dgv2.Size = new System.Drawing.Size(345, 222);
+            this.dgv2.TabIndex = 1;
+            this.dgv2.CellMouseDown += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgv2_CellMouseDown);
+            this.dgv2.DragDrop += new System.Windows.Forms.DragEventHandler(this.dgv2_DragDrop);
+            this.dgv2.DragEnter += new System.Windows.Forms.DragEventHandler(this.dgv2_DragEnter);
+            // 
+            // dgv2Nombre
+            // 
+            this.dgv2Nombre.HeaderText = "Nombre";
+            this.dgv2Nombre.MinimumWidth = 8;
+            this.dgv2Nombre.Name = "dgv2Nombre";
+            this.dgv2Nombre.Width = 150;
+            // 
+            // lbTexto
+            // 
+            this.lbTexto.AutoSize = true;
+            this.lbTexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTexto.Location = new System.Drawing.Point(183, 52);
+            this.lbTexto.Name = "lbTexto";
+            this.lbTexto.Size = new System.Drawing.Size(443, 32);
+            this.lbTexto.TabIndex = 2;
+            this.lbTexto.Text = "Arrastrar un celda de uno a otro";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbTexto);
+            this.Controls.Add(this.dgv2);
+            this.Controls.Add(this.dgv1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgv1Nombre;
+        private System.Windows.Forms.DataGridView dgv2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgv2Nombre;
+        private System.Windows.Forms.Label lbTexto;
+    }
+}
+
---
+++ ArrastrarSoltarCeldasDgv/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrastrarSoltarCeldasDgv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            DataGridViewRow f1Dgv1 = (DataGridViewRow)dgv1.Rows[0].Clone();
+            DataGridViewRow f2Dgv1 = (DataGridViewRow)dgv1.Rows[0].Clone();
+            DataGridViewRow f3Dgv1 = (DataGridViewRow)dgv1.Rows[0].Clone();
+            DataGridViewRow f4Dgv1 = (DataGridViewRow)dgv1.Rows[0].Clone();
+
+            DataGridViewRow f1Dgv2 = (DataGridViewRow)dgv2.Rows[0].Clone();
+            DataGridViewRow f2Dgv2 = (DataGridViewRow)dgv2.Rows[0].Clone();
+            DataGridViewRow f3Dgv2 = (DataGridViewRow)dgv2.Rows[0].Clone();
+            DataGridViewRow f4Dgv2 = (DataGridViewRow)dgv2.Rows[0].Clone();
+
+            f1Dgv1.Cells[0].Value = "Pepe";
+            dgv1.Rows.Add(f1Dgv1);
+            f2Dgv1.Cells[0].Value = "Maria";
+            dgv1.Rows.Add(f2Dgv1);
+            f3Dgv1.Cells[0].Value = "Carlos";
+            dgv1.Rows.Add(f3Dgv1);
+            f4Dgv1.Cells[0].Value = "Valeria";
+            dgv1.Rows.Add(f4Dgv1);
+
+            f1Dgv2.Cells[0].Value = "";
+            dgv2.Rows.Add(f1Dgv2);
+            f2Dgv2.Cells[0].Value = "";
+            dgv2.Rows.Add(f2Dgv2);
+            f3Dgv2.Cells[0].Value = "";
+            dgv2.Rows.Add(f3Dgv2);
+            f4Dgv2.Cells[0].Value = "";
+            dgv2.Rows.Add(f4Dgv2);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Nos permite arrastrar la foto al otro pictureBox
+            this.dgv1.AllowDrop = true;
+            this.dgv2.AllowDrop = true;
+            //Le asignamos que haga lo mismo que el evento que tiene entre parentesis
+            dgv1.CellMouseDown += new DataGridViewCellMouseEventHandler(dgv2_CellMouseDown);
+            dgv1.DragEnter += new DragEventHandler(dgv2_DragEnter);
+            dgv1.DragDrop += new DragEventHandler(dgv2_DragDrop);
+        }
+        //Se produce cuando presionamos el boton izquierdo del raton en una celda
+        private void dgv2_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            //Guardamos en una variable la fila que hemos cogido con el raton
+            int filaCogida = e.RowIndex;
+            //Guardamos en una variable la columna que hemos cogido con el raton
+            int columnaCogida = e.ColumnIndex;
+            //Guardamos en un datagridview la fila y la columna cogida con el raton
+            DataGridView dgv = (DataGridView)sender;
+            dgv.DoDragDrop(dgv.Rows[filaCogida].Cells[columnaCogida].Value.ToString(), DragDropEffects.Copy);
+        }
+        //Se produce cuando se arrastra la celda dentro del datagridview
+        private void dgv2_DragEnter(object sender, DragEventArgs e)
+        {
+            //Si hay una celda se copia en el segundo datagridview y si no pues no
+            DataGridView dgv = (DataGridView)sender;
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        //Se produce cuando se inicia una operacion de arrastrar y colocar
+        private void dgv2_DragDrop(object sender, DragEventArgs e)
+        {
+            DataGridView dgv = (DataGridView)sender;
+            Point clientPoint = dgv2.PointToClient(new Point(e.X, e.Y));
+
+            if (e.Effect == DragDropEffects.Copy)
+            {
+                string cellvalue = e.Data.GetData(typeof(string)) as string;
+                var hittest = dgv2.HitTest(clientPoint.X, clientPoint.Y);
+                if (hittest.ColumnIndex != -1
+                    && hittest.RowIndex != -1)
+                    dgv2[hittest.ColumnIndex, hittest.RowIndex].Value = cellvalue;
+
+            }
+        }
+    }
+}
